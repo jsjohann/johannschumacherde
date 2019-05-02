40dbfc19932563aedf6358600662c62d12ed64be
Johann Schumacher <info@johannschumacher.de>
2019-05-02 17:32:08 +0200

    wip

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var app = new angular.module('Website', []);
+
+app.controller('WebsiteController', function($scope) {
+	$scope.photos = [
+		{"src": "img/1.jpg", "info": ""},
+		{"src": "img/2.jpg", "info": ""},
+		{"src": "img/3.jpg", "info": ""},
+		{"src": "img/4.jpg", "info": ""},
+		{"src": "img/5.jpg", "info": ""},
+		{"src": "img/6.jpg", "info": ""},
+		{"src": "img/7.jpg", "info": ""},
+		{"src": "img/8.jpg", "info": ""},
+		{"src": "img/9.jpg", "info": ""},
+		{"src": "img/10.jpg", "info": ""},
+		{"src": "img/11.jpg", "info": ""},
+		{"src": "img/12.jpg", "info": ""},
+		{"src": "img/13.jpg", "info": ""},
+	];
+});
